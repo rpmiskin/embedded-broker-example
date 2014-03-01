70f93eacddb70d1d9cd3702e6264dfdeb0e68646
Richard Miskin <r.p.miskin@gmail.com>
2014-03-01 17:54:57 +0000

    Minor updates to the example

--- src/main/java/in/misk/timed/route/TimedRoute.java
+++ src/main/java/in/misk/timed/route/TimedRoute.java
@@ -7,33 +7,42 @@ import org.apache.camel.Expression;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.stereotype.Component;
 
+/**
+ * Example that shows the use of the embedded broker.
+ * 
+ * <p>
+ * Also shows the difference between using a queue vs direct when using a timer.
+ * With the queue if the consumer is slow running multiple messages can build up
+ * in the queue so you see ping, ping, pong, pong rather than ping, pong, ping,
+ * pong.
+ * </p>
+ * <p>
+ * When using direct the timer does not fire again until the slow consumer has
+ * completed.
+ * </p>
+ */
 @Component
 public class TimedRoute extends SpringRouteBuilder {
 
-	private static final String QUEUE = "embedded:destination";
-	// private static final String QUEUE = "direct:destination";
+	private static final String QUEUE = "embedded:a.queue";
+	// private static final String QUEUE = "direct:not.a.queue";
 	Random r = new Random();
 
 	@Override
 	public void configure() throws Exception {
-		from("timer://foo?fixedRate=false&period=1000").log("foo!").setBody()
-				.constant("body").to(QUEUE);
+		from("timer://foo?period=100").log("ping").to(QUEUE);
 
-		from(QUEUE).log("Before").setHeader("delay", getDelay())
-				// .log("delay: ${header.delay}")
-				.delay(simple("${header.delay}")).log("---------------bar!")
-				.stop();
+		from(QUEUE).delay(getDelay()).log("        pong!").stop();
 
 	}
 
+	/** Creates an expression for a random delay. */
 	private Expression getDelay() {
-		// TODO Auto-generated method stub
 		return new Expression() {
 
 			@Override
 			public <T> T evaluate(final Exchange arg0, final Class<T> arg1) {
-				final Long l = Long.valueOf(10000);// Long.valueOf(r.nextInt(500)
-													// + 85);
+				final Long l = Long.valueOf(r.nextInt(100) + 50);
 				T retVal = null;
 				if (arg1.isAssignableFrom(l.getClass())) {
 					retVal = arg1.cast(l);
